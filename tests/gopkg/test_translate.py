7f5b9cf2df2ee90a435552335094853bf74ffd66
Martin Sehnoutka <msehnout@redhat.com>
2019-01-16 15:55:44 +0100

    docstrings for gopkg tests

--- tests/gopkg/test_translate.py
+++ tests/gopkg/test_translate.py
@@ -1,12 +1,16 @@
+"""Test the gopkg module."""
+
 from ghmonitor.gopkg.translate import *
 
 
 def test_get_repo_urn():
+    """Test the function. Be careful though, it requires an Internet access."""
     assert 'kubernetes/metrics' == get_repo_from_random_urn('k8s.io/metrics')
     assert get_repo_from_random_urn('seznam.cz') is None
 
 
 def test_github_re():
+    """Test the regular expression."""
     assert GITHUB_REPO_RE.match('bitbucket.com/user/repo') is None
     m = GITHUB_REPO_RE.match('github.com/user/project')
     assert m.group('user') == 'user'
@@ -17,6 +21,7 @@ def test_github_re():
 
 
 def test_translate():
+    """Test the translation. Again, needs Internet access."""
     assert 'kubernetes/metrics' == translate('k8s.io/metrics')
     assert 'user/project' == translate('github.com/user/project')
     assert translate('launchpad.net/project') is None
