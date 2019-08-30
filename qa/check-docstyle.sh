1085bc048a9a1b307444f1bfa4f741d9836f6a6b
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-30 08:24:38 +0200

    Updated the script check-docstyle.sh to be able to run from qa subdirectory

--- qa/check-docstyle.sh
+++ qa/check-docstyle.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Script to check all Python scripts for PEP-8 issues
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 IFS=$'\n'
 
 # list of directories with sources to check
-directories=$(cat directories.txt)
+directories=$(cat ${SCRIPT_DIR}/directories.txt)
 
 # list of separate files to check
-separate_files=$(cat files.txt)
+separate_files=$(cat ${SCRIPT_DIR}/files.txt)
 
 pass=0
 fail=0
@@ -36,6 +40,8 @@ function prepare_venv() {
 
     ${PYTHON} -m venv "venv" && source venv/bin/activate && python -m pip install pydocstyle >&2
 }
+
+pushd "${SCRIPT_DIR}/.."
 
 # run the pydocstyle for all files that are provided in $1
 function check_files() {
@@ -85,6 +91,7 @@ echo "----------------------------------------------------"
 
 check_files "$separate_files"
 
+popd
 
 if [ $fail -eq 0 ]
 then
